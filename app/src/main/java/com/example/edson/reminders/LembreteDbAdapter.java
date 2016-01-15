3936ba4287eb2e7949a7e41b2497f95e30f698e8
edson almeida <almeidaesa@gmail.com>
2016-01-15 10:02:14 -0200

    conclusao do crud

---
+++ app/src/main/java/com/example/edson/reminders/LembreteDbAdapter.java
@@ -0,0 +1,7 @@
+package com.example.edson.reminders;
+
+/**
+ * Created by edson on 13/01/2016.
+ */
+public class LembreteDbAdapter {
+}
---
+++ app/src/main/java/com/example/edson/reminders/LembreteSimpleCursorAdapter.java
@@ -0,0 +1,7 @@
+package com.example.edson.reminders;
+
+/**
+ * Created by edson on 14/01/2016.
+ */
+public class LembreteSimpleCursorAdapter {
+}
